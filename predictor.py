8bc274a409199103f694e442bc2c585e919c9c2f
Nebijk <130664604+nebijk@users.noreply.github.com>
2024-09-11 21:22:18 +0200

    testing the accuracy

--- predictor.py
+++ predictor.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 games = pd.read_csv("games.csv",index_col=0)
 print(games.head())
@@ -20,3 +21,16 @@ print(games)
 games["hour"] = games["time"].str.replace(":.+","", regex=True).astype("int")
 games["day_code"] = games["date"].dt.day_of_week
 print(games)
+
+games["goal"]= (games["result"] == "W").astype("int")
+randF=RandomForestClassifier(n_estimators=50,min_samples_split=10, random_state=1)
+train=games[games["date"]<'2022-01-01']
+test=games[games["date"]>'2022-01-01']
+
+predictors=["arena_code", "vs_code","hour","day_code"]
+randF.fit(train[predictors],train["goal"])
+
+preds = randF.predict(test[predictors])
+acc=accuracy_score(test["goal"], preds)
+
+print(acc)
