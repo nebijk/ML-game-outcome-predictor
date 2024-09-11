0c2bdf13beae83e51d1251a1f91a7eaf497c374d
Nebijk <130664604+nebijk@users.noreply.github.com>
2024-09-11 20:58:03 +0200

    adding data

--- predictor.py
+++ predictor.py
@@ -8,3 +8,15 @@ print(games["team"].value_counts())
 print(games[games["team"] == "Liverpool"])
 print(games["round"].value_counts())
 print(games.dtypes)
+
+games["date"] = pd.to_datetime(games["date"])
+print(games.dtypes)
+
+games["arena_code"] = games["venue"].astype("category").cat.codes
+print(games)
+games["vs_code"] = games["opponent"].astype("category").cat.codes
+print(games)
+
+games["hour"] = games["time"].str.replace(":.+","", regex=True).astype("int")
+games["day_code"] = games["date"].dt.day_of_week
+print(games)
